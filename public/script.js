5cc6a1fcbf14754f20520d502f6cbadf6d473a00
oguilhermefigueiredo <negocio.guilhermefigueiredo@gmail.com>
2022-02-08 10:36:21 -0300

    Implementing the HTML page and the PostgreSQL database container

---
+++ public/script.js
@@ -0,0 +1,12 @@
+fetch("/employees")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((employee) => {
+      const cardTemplate = document.querySelector("template");
+      const card = cardTemplate.content.cloneNode(true);
+      card.querySelector("h4").innerText = employee.name;
+      card.querySelector("p").innerText = employee.title;
+      document.body.appendChild(card);
+    });
+  });
