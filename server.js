3d9c60d61f87ff93cb1999ed0d0c154324277063
oguilhermefigueiredo <negocio.guilhermefigueiredo@gmail.com>
2022-02-08 05:56:12 -0300

    Creating npm project and setting the server with Express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", async (req, res) => {
+  res.setHeader("Content-type", "text/html");
+  res.status(200);
+  res.send("<h1>Server is running...</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Actually listen at the ${port} port`);
+});
